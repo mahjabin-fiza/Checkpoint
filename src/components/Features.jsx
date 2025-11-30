bf38d4708880f0631cd86a99deeb8fc7e5c893ce
Medha <zahna.medha@northsouth.edu>
2025-11-30 23:07:13 +0600

    Removed Firebase secret files and updated gitignore

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Feature() {
   return (
     <div className='w-full h-full flex justify-center items-start'>
-      <div><Medha></Medha></div>
+      <div>Features</div>
     </div>
   )
 }
