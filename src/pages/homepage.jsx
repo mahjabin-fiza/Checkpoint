b332897ac37b49aab8fa931bbae63af0aeb7fd97
Medha <zahna.medha@northsouth.edu>
2025-12-01 01:09:30 +0600

    Fix case rename for Homepage.jsx

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import Header from '../components/Header.jsx';
-import Search from '../components/Search.jsx';
-import Features from '../components/Features.jsx';
+import Header from '../components/Header';
+import HotelSearch from '../components/HotelSearch';
 
-function Homepage() {
-  return (
-    <div>
-      <>
-        <Header />
-        <div className='p-7 flex flex-col gap-7'>
-          <div><Search /></div>
-          <div><Features /></div>
-        </div>
-      </>
-    </div>
-  );
+function Home () {
+    return(
+        <>
+        <Header/>
+        <HotelSearch/>
+        </>
+    );
 }
 
-export default Homepage;
+export default Home;
